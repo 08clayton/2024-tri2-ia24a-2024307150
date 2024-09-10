2d7f9e8de40175e2d879502494ff2f10435bc61a
08clayton <claytonfilhosilva@gmail.com>
2024-09-10 11:42:34 +0000

    zica

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,10 @@
 //seleciona uma lista com todos os buttons com a classe toggle-nav
-const btsToggleNav = document.querySelectorAll('button.toggle-nav')
-const nv = document.querySelector('nav')
+  const btsToggleNav = document.querySelectorAll('button.toggle-nav')
+  const nv = document.querySelector('nav')//seleciona o primeiro elemento do nav que ele encontrar
 
-btsToggleNav.forEach(function(bt){
-  bt.addEventListener('click', function(){
-    nv.classList.toggle('open')
+//o forEach é usado para iterar sobre cada botão dentro do node list para cada botão
+    btsToggleNav.forEach(function(bt){
+    bt.addEventListener('click', function(){ //add adiciona um ouvinte de de eventos que escuta o 'click' qnd for clicado
+nv.classList.toggle('open')//qnd for clicado o código usa classList(adicions s classe 'open' aos elementos do nav)
   })
 })
