8ae2efbcc96588fc46b9fc42b45d6b5a29203d59
08clayton <claytonfilhosilva@gmail.com>
2024-09-24 14:18:01 +0000

    top

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,27 @@
-//seleciona uma lista com todos os buttons com a classe toggle-nav
-  const btsToggleNav = document.querySelectorAll('button.toggle-nav')
-  const nv = document.querySelector('nav')//seleciona o primeiro elemento do nav que ele encontrar
+const domBtsToggleNav = document.querySelectorAll('button.toggle-nav')
+const domNav = document.querySelector('nav')
 
-//o forEach é usado para iterar sobre cada botão dentro do node list para cada botão
-    btsToggleNav.forEach(function(bt){
-    bt.addEventListener('click', function(){ //add adiciona um ouvinte de de eventos que escuta o 'click' qnd for clicado
-nv.classList.toggle('open')//qnd for clicado o código usa classList(adicions s classe 'open' aos elementos do nav)
+domBtsToggleNav.forEach(function (bt) {
+  bt.addEventListener('click', function () {
+    domNav.classList.toggle('open')
   })
 })
+
+// ---------------------
+
+async function listarPersonagens() {
+  const template = document.querySelector('#modelo-personagem')
+  const request = await fetch('personagens.json')
+  const dados = await request.json()
+
+  dados.forEach(personagem => {
+    const novoDomPersonagem = template.content.cloneNode(true)
+    novoDomPersonagem.querySelector('h2').textContent = personagem.nome
+    novoDomPersonagem.querySelector('p').textContent = personagem.classe
+    template.parentNode.appendChild(novoDomPersonagem)
+  })
+}
+
+listarPersonagens()
+
+// https://rickandmortyapi.com/api/character
